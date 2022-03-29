21c76097569e4a0207af04224b83f39679fd6cee
inzh <jiahao.king@outlook.com>
2022-03-29 17:27:48 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: '/idphotomaker/',
+  publicPath: './',
 })
